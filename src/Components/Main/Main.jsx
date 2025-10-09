94b015baf5bcc548ee31874b334628a9402a8861
Muhammad Suffyan Abbasi <Ahmed@capiplan.com>
2025-10-09 17:05:12 +0500

    added my skill set

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,8 +1,9 @@
 // import profileImg from "../../assets/(4).jpg";
-import reactIMG from "../../assets/reactImage.jpg";
 import HTML from "../../assets/htmlIMG.webp";
 import CSS from "../../assets/cssImg.webp";
 import JavaScript from "../../assets/javaScriptImg.png";
+import reactIMG from "../../assets/reactImage.jpg";
+import next from "../../assets/nextImg.png";
 import Express from "../../assets/expressIMG.png";
 import Nest from "../../assets/nestImg.png";
 import MonogDB from "../../assets/monogDB.png";
@@ -16,17 +17,17 @@ const Main = () => {
     <>
       <div className="main w-full py-2 bg-danger  ">
         <h2 className="skills  text-6xl font-[600]">My Technology Stack</h2>
-        <div className="  w-full flex gap-6 flex-wrap justify-center items-center  my-5 px-4">
+        <div className="  w-full flex gap-6 flex-wrap justify-center items-center  my-10 px-4">
           <div class="flip-card card-one">
             <div className="flip-card-inner flip-card-inner-one">
               <div className="flip-card-front">
                 <img src={HTML} className="img" alt="" />
               </div>
-              <div className="flip-card-back">
-                <div className="text-dark">
-                  <h1>John Doe</h1>
-                  <p>Architect & Engineer</p>
-                  <p>We love that guy</p>
+              <div className="flip-card-back back-one flex items-center justify-center">
+                <div className="flex justify-center items-center">
+                  <h1 className="text-6xl font-[900] text-[#1E1E1E] html-text">
+                    HTML
+                  </h1>
                 </div>
               </div>
             </div>
@@ -36,25 +37,39 @@ const Main = () => {
               <div className="flip-card-front">
                 <img src={CSS} className="img" alt="" />
               </div>
-              <div className="flip-card-back">
-                <div className="text-dark">
-                  <h1>John Doe</h1>
-                  <p>Architect & Engineer</p>
-                  <p>We love that guy</p>
+              <div className="flip-card-back back-two flex items-center justify-center">
+                <div className="flex justify-center items-center">
+                  <h1 className="text-6xl font-[900] text-[#FFFFFF] css-text">
+                    CSS
+                  </h1>
                 </div>
               </div>
             </div>
           </div>
-          <div class="flio-card card-four">
-            <div className="flip-card-inner flip-card-inner-four">
+          <div class="flio-card card-three">
+            <div className="flip-card-inner flip-card-inner-three">
               <div className="flip-card-front">
                 <img src={JavaScript} className="img" alt="" />
               </div>
-              <div className="flip-card-back">
-                <div className="text-dark">
-                  <h1>John Doe</h1>
-                  <p>Architect & Engineer</p>
-                  <p>We love that guy</p>
+              <div className="flip-card-back back-three flex items-center justify-center">
+                <div className="flex justify-center items-center">
+                  <h1 className="text-6xl font-[900] text-[#2C2C2C] javascript-text">
+                    Java Script
+                  </h1>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="flip-card card-four">
+            <div className="flip-card-inner flip-card-inner-four">
+              <div className="flip-card-front">
+                <img src={reactIMG} className="img" alt="" />
+              </div>
+              <div className="flip-card-back back-four flex items-center justify-center">
+                <div className="flex justify-center items-center">
+                  <h1 className="text-6xl font-[900] text-[#0A3D62] react-text">
+                    React
+                  </h1>
                 </div>
               </div>
             </div>
@@ -62,13 +77,13 @@ const Main = () => {
           <div class="flip-card card-five">
             <div className="flip-card-inner flip-card-inner-five">
               <div className="flip-card-front">
-                <img src={reactIMG} className="img" alt="" />
+                <img src={next} className="img" alt="" />
               </div>
-              <div className="flip-card-back">
-                <div className="text-dark">
-                  <h1>John Doe</h1>
-                  <p>Architect & Engineer</p>
-                  <p>We love that guy</p>
+              <div className="flip-card-back back-five  flex items-center justify-center">
+                <div className="flex justify-center items-center">
+                 <h1 className="text-6xl font-[900] text-[#222222] next-text">
+                    Next
+                  </h1>
                 </div>
               </div>
             </div>
@@ -78,11 +93,11 @@ const Main = () => {
               <div className="flip-card-front">
                 <img src={Express} className="img" alt="" />
               </div>
-              <div className="flip-card-back">
-                <div className="text-dark">
-                  <h1>John Doe</h1>
-                  <p>Architect & Engineer</p>
-                  <p>We love that guy</p>
+              <div className="flip-card-back back-six flex justify-center items-center">
+                <div className="flex justify-center items-center">
+                  <h1 className="text-6xl font-[900] text-[#222222] next-text">
+                    Expres
+                  </h1>
                 </div>
               </div>
             </div>
@@ -92,11 +107,11 @@ const Main = () => {
               <div className="flip-card-front">
                 <img src={Nest} className="img" alt="" />
               </div>
-              <div className="flip-card-back">
-                <div className="text-dark">
-                  <h1>John Doe</h1>
-                  <p>Architect & Engineer</p>
-                  <p>We love that guy</p>
+              <div className="flip-card-back back-seven flex justify-center items-center">
+                <div className="flex justify-center items-center">
+                   <h1 className="text-6xl font-[900] text-[#FFFFFF] nest-text">
+                    Nest
+                  </h1>
                 </div>
               </div>
             </div>
@@ -106,11 +121,11 @@ const Main = () => {
               <div className="flip-card-front">
                 <img src={MonogDB} className="img" alt="" />
               </div>
-              <div className="flip-card-back">
-                <div className="text-dark">
-                  <h1>John Doe</h1>
-                  <p>Architect & Engineer</p>
-                  <p>We love that guy</p>
+              <div className="flip-card-back back-eight flex justify-center items-center">
+                <div className="flex justify-center items-center">
+                   <h1 className="text-6xl font-[900] text-[#FFFFFF] mongo-text">
+                    Mongo DB
+                  </h1>
                 </div>
               </div>
             </div>
@@ -120,25 +135,25 @@ const Main = () => {
               <div className="flip-card-front">
                 <img src={MySQL} className="img" alt="" />
               </div>
-              <div className="flip-card-back">
-                <div className="text-dark">
-                  <h1>John Doe</h1>
-                  <p>Architect & Engineer</p>
-                  <p>We love that guy</p>
+              <div className="flip-card-back back-nine flex justify-center items-center">
+                <div className="flex justify-center items-center">
+                  <h1 className="text-6xl font-[900] text-[#FFFFFF] my-text">
+                    My SQL
+                  </h1>
                 </div>
               </div>
             </div>
           </div>
           <div class="flip card-ten">
-            <div className="flip-card-inner flip-card-inner-ten">
+            <div className="flip-card-inner flip-card-inner-ten ">
               <div className="flip-card-front">
                 <img src={PostGres} className="img" alt="" />
               </div>
-              <div className="flip-card-back">
-                <div className="text-dark">
-                  <h1>John Doe</h1>
-                  <p>Architect & Engineer</p>
-                  <p>We love that guy</p>
+              <div className="flip-card-back back-ten flex justify-center items-center">
+                <div className="flex justify-center items-center">
+                  <h1 className="text-6xl font-[900] text-[#FFFFFF] my-text">
+                    Post Gres
+                  </h1>
                 </div>
               </div>
             </div>
@@ -148,11 +163,9 @@ const Main = () => {
               <div className="flip-card-front">
                 <img src={GitHub} className="img" alt="" />
               </div>
-              <div className="flip-card-back">
-                <div className="text-dark">
-                  <h1>John Doe</h1>
-                  <p>Architect & Engineer</p>
-                  <p>We love that guy</p>
+              <div className="flip-card-back back-eleven flex justify-center items-center">
+                <div className="flex justify-center items-center">
+                  <h1 className="text-6xl font-[900] text-[#222222] next-text">GitHub</h1>
                 </div>
               </div>
             </div>
@@ -162,11 +175,9 @@ const Main = () => {
               <div className="flip-card-front">
                 <img src={GitLab} className="img" alt="" />
               </div>
-              <div className="flip-card-back">
-                <div className="text-dark">
-                  <h1>John Doe</h1>
-                  <p>Architect & Engineer</p>
-                  <p>We love that guy</p>
+              <div className="flip-card-back back-twelve flex justify-center items-center">
+                <div className="flex justify-center items-center">
+                   <h1 className="text-6xl font-[900] text-[#FFFFFF] lab-text">GitLab</h1>
                 </div>
               </div>
             </div>
